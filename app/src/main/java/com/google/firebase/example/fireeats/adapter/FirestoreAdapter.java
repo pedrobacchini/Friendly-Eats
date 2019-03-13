13ec5753c653a79101aaf66a44456117bfa55b4b
Pedro Bacchini <pedrobacchini@outlook.com>
2019-03-13 15:51:30 -0300

    End of tutorial

--- app/src/main/java/com/google/firebase/example/fireeats/adapter/FirestoreAdapter.java
+++ app/src/main/java/com/google/firebase/example/fireeats/adapter/FirestoreAdapter.java
@@ -16,13 +16,19 @@
  package com.google.firebase.example.fireeats.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+
+import javax.annotation.Nullable;
 
 /**
  * RecyclerView adapter for displaying the results of a Firestore {@link Query}.
@@ -35,7 +41,7 @@ import java.util.ArrayList;
  * more efficient implementation of a Firestore RecyclerView Adapter.
  */
 public abstract class FirestoreAdapter<VH extends RecyclerView.ViewHolder>
-        extends RecyclerView.Adapter<VH> {
+        extends RecyclerView.Adapter<VH> implements EventListener<QuerySnapshot> {
 
     private static final String TAG = "Firestore Adapter";
 
@@ -44,12 +50,12 @@ public abstract class FirestoreAdapter<VH extends RecyclerView.ViewHolder>
 
     private ArrayList<DocumentSnapshot> mSnapshots = new ArrayList<>();
 
-    public FirestoreAdapter(Query query) {
-        mQuery = query;
-    }
+    public FirestoreAdapter(Query query) { mQuery = query; }
 
     public void startListening() {
-        // TODO(developer): Implement
+        if(mQuery != null && mRegistration == null) {
+            mRegistration = mQuery.addSnapshotListener(this);
+        }
     }
 
     public void stopListening() {
@@ -60,6 +66,60 @@ public abstract class FirestoreAdapter<VH extends RecyclerView.ViewHolder>
 
         mSnapshots.clear();
         notifyDataSetChanged();
+    }
+
+    @Override
+    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+        // Handle erros
+        if(e != null) {
+            Log.w(TAG, "onEvent:error", e);
+            return;
+        }
+
+        assert queryDocumentSnapshots != null;
+
+        // Dispatch the event
+        for(DocumentChange change : queryDocumentSnapshots.getDocumentChanges()) {
+            // Snapshot of the changed document
+            DocumentSnapshot snapshot = change.getDocument();
+
+            switch(change.getType()) {
+                case ADDED:
+                    onDocumentAdded(change);
+                    break;
+                case MODIFIED:
+                    onDocumentModified(change);
+                    break;
+                case REMOVED:
+                    onDocumentRemoved(change);
+                    break;
+            }
+        }
+
+        onDataChanged();
+    }
+
+    protected void onDocumentAdded(DocumentChange change) {
+        mSnapshots.add(change.getNewIndex(), change.getDocument());
+        notifyItemInserted(change.getNewIndex());
+    }
+
+    protected void onDocumentModified(DocumentChange change) {
+        if(change.getOldIndex() == change.getNewIndex()) {
+            // Item changed but remained in same position
+            mSnapshots.set(change.getOldIndex(), change.getDocument());
+            notifyItemChanged(change.getOldIndex());
+        } else {
+            // Item changed and changed position
+            mSnapshots.remove(change.getOldIndex());
+            mSnapshots.add(change.getNewIndex(), change.getDocument());
+            notifyItemMoved(change.getOldIndex(), change.getNewIndex());
+        }
+    }
+
+    protected void onDocumentRemoved(DocumentChange change) {
+        mSnapshots.remove(change.getOldIndex());
+        notifyItemRemoved(change.getOldIndex());
     }
 
     public void setQuery(Query query) {
